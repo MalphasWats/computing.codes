979d352af7171f66124494c3e07be583e6bada2c
Mike Watts <malphas@subdimension.co.uk>
2020-03-22 15:13:02 +0000

    spaces

--- JDojoMaster/src/covid19/Scene.java
+++ JDojoMaster/src/covid19/Scene.java
@@ -5,17 +5,17 @@ import java.awt.Dimension;
 
 public class Scene extends JPanel {
 
-	static final long serialVersionUID = 1;
+    static final long serialVersionUID = 1;
 
-	private int width;
-	private int height;
+    private int width;
+    private int height;
 
-	public Scene() {
-		super();              // Make sure to call the JPanel constructor
+    public Scene() {
+        super();              // Make sure to call the JPanel constructor
 
-		width = 256;          // Default width
-		height = 256;         // Default height
-	}
+        width = 256;          // Default width
+        height = 256;         // Default height
+    }
 
     public Dimension getPreferredSize() {
         return new Dimension(this.width,this.height);
--- JDojoMaster/src/covid19/Sprite.java
+++ JDojoMaster/src/covid19/Sprite.java
@@ -6,19 +6,19 @@ import java.awt.Color;
 
 public class Sprite {
 
-	protected BufferedImage image;
-	protected int x;
-	protected int y;
+    protected BufferedImage image;
+    protected int x;
+    protected int y;
 
     /* Default constructor */
-	public Sprite() {
-		x = 0;
-		y = 0;
+    public Sprite() {
+        x = 0;
+        y = 0;
 
-		image = new BufferedImage(32, 32, BufferedImage.TYPE_INT_RGB);
-		Graphics g = image.getGraphics();
+        image = new BufferedImage(32, 32, BufferedImage.TYPE_INT_RGB);
+        Graphics g = image.getGraphics();
 
         g.setColor(new Color(255, 0, 255));
         g.fillRect(0, 0, 32, 32);
-	}
+    }
 }
